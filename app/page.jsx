1b0c7a95c496867a979187e794e54f8c57c0dd70
rajesh <peketirajesh007@gmail.com>
2024-02-21 19:35:38 +0530

    notify

--- app/page.jsx
+++ app/page.jsx
@@ -2,16 +2,21 @@ import Image from "next/image";
 import Bg from "../public/assets/Preview.jpg"
 import Buttons from "@/components/commons/Buttons";
 import Link from "next/link";
-import Title from "@/components/commons/Title";
+import Chat from "/public/assets/chat.svg"
 
 
 export default function Home() {
   return (
     <>
-      <main className="h-screen  relative w-full">
+      <main className="h-screen relative w-full overflow-x-hidden">
         <Image src={Bg} alt="chat" width={1000} height={1000} className="h-full w-full absolute object-cover object-left " />
         <div className="backdrop-blur h-full w-full flex flex-col justify-center items-center">
-          <Title text={"TalkNow"} styles={"-right-6 top-0"} />
+          {/* <Title text={"TalkNow"} styles={"-right-6 top-0"} /> */}
+          <h1 className="relative mb-[10px] ">
+            <span className="font-bold text-[40px] font-courgette ">TalkNow</span>
+            <Image src={Chat} alt="chat" width={22} height={22} className='absolute top-0 -right-6' />
+          </h1>
+
           <div className="text-center mb-[180px]">
             <p className="text-secondary text-2xl font-semibold p-1">Welcome to talkNow!</p>
             <p className="">Stay Connected Anytime, Anywhere!</p>
